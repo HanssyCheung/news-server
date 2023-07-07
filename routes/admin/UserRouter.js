c17307cd75ec75cb74f45acfefb925f239a86d0a
cheunghanssy <cheunghanssy@163.com>
2023-07-07 18:17:10 +0800

    center页面接口准备

--- routes/admin/UserRouter.js
+++ routes/admin/UserRouter.js
@@ -3,5 +3,6 @@ const UserController = require('../../controllers/admin/UserController');
 var UserRouter = express.Router();
 
 UserRouter.post("/adminapi/user/login",UserController.login)//登录接口
+UserRouter.post("/adminapi/user/upload",UserController.upload)//登录接口
 
 module.exports = UserRouter;
